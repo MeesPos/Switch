34c25fee915ccac286964d7835ef25d0127a01ed
MeesPos <meespostma_@hotmail.com>
2020-03-25 13:42:36 +0100

    Rippel toegevoegd

--- ripple.js
+++ ripple.js
@@ -2,10 +2,21 @@ const alleVlakken = document.querySelectorAll('.vlak');
 const rimpel = document.createElement('div');
 rimpel.classList = 'rimpel';
 
-function maakRipple(e) {
-    console.log(e);
-}
+for (let i = 0; i < alleVlakken.length; i++) {
+    alleVlakken[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        let x = parseInt(e.pageX - this.offsetLeft - 50);
+        let y = e.pageY - this.offsetTop - 50;
+        rimpel.style.left = x + 'px';
+        rimpel.style.top = y + 'px';
+        e.target.append(rimpel);
+        rimpel.classList.add('groeien');
 
-for(let i=0; i<alleVlakken.length; i++) {
-    alleVlakken[i].addEventListener('click', maakRipple);
+        // Als er geen checkbox is, deze omzetten
+        if (this.getElementsByTagName('input')[0]) {
+            let deCB = this.getElementsByTagName('input')[0];
+            deCB.checked = !deCB.checked;
+        }
+        
+    });
 }
